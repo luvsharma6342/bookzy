cd25b891f044ac102e7b9888118594d603691512
Luv Sharma <luvsharma105@gmail.com>
2026-06-14 22:06:57 +0530

    fix/google-auth

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,8 +2,6 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
-console.log("process.env.DATABASE_URL: ", process.env.DATABASE_URL);
-
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
