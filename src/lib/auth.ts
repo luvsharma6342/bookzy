a649f860c4947876bb342852ab5eb75944a2bfa4
Luv Sharma <luvsharma105@gmail.com>
2026-06-14 12:05:44 +0530

    feat/google-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,10 +3,18 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      prompt: "select_account",
+    },
   },
 });
